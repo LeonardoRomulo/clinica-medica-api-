b9f2cab2d7c0b94d07a648121d665b60a739398e
LeonardoRomulo <leo.romulo15@gmail.com>
2025-08-20 18:43:59 -0300

    feat: implementação de tratamento de erros na api

--- app.js
+++ app.js
@@ -6,13 +6,24 @@ const app = express();
 app.use(cors());
 
 app.get('/medicos', async (req, res)  => {
-    let listaMedica;
-    listaMedica = await retornaListaMedica();
-    res.json(listaMedica);
+    try {
+        const listaMedica = await retornaListaMedica();
+        if(!listaMedica || listaMedica.length === 0){
+            return res.status(404).json({mensagem: 'Nenhum médico encontrado'});
+        }
+        res.json(listaMedica);
+    }catch(error) {
+        res.status(500).json({mensagem: 'Erro ao buscar a lista médica'});
+    }
+
+    
 })
 
+app.use((req,res) => {
+    res.status(404).json({mensagem: 'Rota não encontrada'});
+});
 
 app.listen (9000, async () => {
     const date = new Date();
     console.log(`Servidor iniciado em: ${date}`);
-})
+});
